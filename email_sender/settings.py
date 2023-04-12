460f06193ebc5b3a1debaa0246c7cc8ab3d00109
Saimon Zinchenko <s.zinchenko@ktsstudio.com>
2023-04-12 20:49:26 +0300

    wip

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-c!chpc7(3wriet-hj9cc$1&()=k&d_7yqj9r=_(pebw9r1#yt-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["91.201.54.155",]
 
 
 # Application definition
